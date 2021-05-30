07f8beefbea49836bbde482dab883fb7210ac4de
suneet <sunit.jena.sj@gmail.com>
2021-05-30 07:47:04 +0530

    button comment updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,8 @@ import React, { useContext } from 'react'
 import LanguageContext from '../contexts/LanguageContext'
 import ColorContext from '../contexts/ColorContext'
 
-const Button = () => {//branch changed - add color 
-    //this button is related to field
-    const y =  useContext(LanguageContext);
+const Button = () => {//button_comment - add color 
+     const y =  useContext(LanguageContext);
     const x =  useContext(ColorContext);
 
     return <button className={`ui button ${x}`} >

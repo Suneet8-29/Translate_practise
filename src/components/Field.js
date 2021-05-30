daab89e569d94b5b581dc066eee0987cac66db54
suneet <sunit.jena.sj@gmail.com>
2021-05-30 07:32:01 +0530

    field comment updated

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Field = () => {//Display name into ui 
+const Field = () => {//Field_comment : Display name onto ui
     return (
         <div className='ui field'>
             <label>Name</label>
